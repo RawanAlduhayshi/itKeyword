b6ddf720900a56388626d56ed7d9d9e5ad6c02f6
Rawan <Rawan.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-18 09:10:57 +0300

    Using it keyword

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+   //using it keyword
+    val greetingFunction : (String )-> String ={
+        val currentYear = 2018
+        "Welcome to SimVillage,$it! (copyright) $currentYear"
+    }
+    println(greetingFunction("Rawan"))
+}
